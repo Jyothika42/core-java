0f65433ee537917b268f555d95d31a2cbcc16f74
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 19:05:34 +0530

    basic code

---
+++ Sunscreen.java
@@ -0,0 +1,11 @@
+class Sunscreen {
+
+	public static void main(String[] Derma){
+		System.out.println("6-HOUR LONG LASTING FORMULA: Powered with Heat Shield Technology, this sunscreen defends your skin from sun damage for upto 6 hours and protects your skin from environmental aggressors. So, no need for reapplication.");
+		System.out.println("WATER & SWEAT RESISTANT: Come sweat or rain, this long-lasting sunscreen stays put and protects your skin from sun damage throughout the day.");
+		System.out.println("NON-GREASY & LIGHTWEIGHT: Greasiness or heaviness? No chance. This sunscreen feels light as air on the skin and blends effortlessly without leaving any residue");
+		System.out.println("WHO IS IT SUITABLE FOR? Want long lasting protection sun protection? This sunscreen is perfect for you! It's dermatologically tested and is ideal for all skin types.");
+		
+	}
+
+}
