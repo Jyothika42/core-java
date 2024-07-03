532e45bc9648fc564539388f252a9ee2ed541f11
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 18:17:19 +0530

    my first basic code on TV

---
+++ SmartLEDGoogleTV.java
@@ -0,0 +1,12 @@
+class SmartLEDGoogleTV {
+	
+	public static void main(String[] panasonic) {
+		System.out.println("Resolution : 4K Ultra HD (3840 x 2160) | Refresh Rate : 60 Hertz");
+		System.out.println("Connectivity: 3 HDMI ports to connect set top box, Blu-ray speakers or gaming console | 2 USB ports to connect hard drives or other USB devices | Bluetooth | Built-in Wi-Fi");
+		System.out.println("Sound: 20 Watts Output | Dolby Digital | Audio Booseter+");
+		System.out.println("Smart TV Features: Google TV | In-Built WiFi | Screen Mirroring | 2 GB RAM | 16 GB ROM | Supported Apps: Netflix, Prime Video, YouTube, Zee5, etc. | Google Assistant Operation");
+		System.out.println("Display: 4K HDR | Wide Viewing Angle | Micro Dimming | Noise Reduction | Hexa Chroma Drive | 4K Upscaling | 4K Studio Color Engine");
+		
+	}
+
+}
