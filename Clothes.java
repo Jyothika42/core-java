f35a536145004f5dfad2d673ebd0ac3082ce9c00
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 19:07:38 +0530

    basic code

---
+++ Clothes.java
@@ -0,0 +1,13 @@
+class Clothes {
+
+	public static void main (String[] Puma){
+		
+		// display any messages
+		System.out.println("Clothes are trendy these days");
+		System.out.println("Clothes are important on Fastivals");
+		System.out.println("Clothes are for personal use");
+		System.out.println("Clothes are made to design");
+		System.out.println("Clothes are worn on body");
+	}
+		
+}
