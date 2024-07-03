90066b7988ff04c8cb40715d22311111d0a3e2ae
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 18:44:01 +0530

    basic code

---
+++ LEDBulb.java
@@ -0,0 +1,14 @@
+class LEDBulb{
+	
+	public static void main(String[] philips) {
+		System.out.println("Includes LED Bulb");
+		System.out.println("Wattage: 9 watts");
+		System.out.println("Warranty: 1 year on product from date of invoice");
+		System.out.println("Up to 40 percent energy saving over 15W CFL");
+		System.out.println("Base Cap: B22");
+		System.out.println("Long life of 10 years with 10000 hours life");
+		System.out.println("Up to 85 percent energy saving compared to GLS");
+		System.out.println("Available in 9W as a replacement for 15W CFL");
+	}
+
+}
