3081220d14e5975e9f900a27b27067a1a84e5d25
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 18:56:36 +0530

    basic code

---
+++ FaceWash.java
@@ -0,0 +1,12 @@
+class FaceWash {
+	
+	public static void main(String[] BodyWise) {
+		System.out.println("DEEP CLEANSES SKIN: Formulated with 2% Salicylic Acid - a potent BHA - this face wash exfoliates dead skin cells and removes excess oil, dirt and other impurities.");
+		System.out.println("PREVENTS ACNE FORMATION: Salicylic acid unclogs pores and regulates sebum production thereby reducing acne formation.");
+		System.out.println("DREDUCES BLACKHEADS: Blackheads are caused due to clogged pores. This facial cleanser with salicylic acid unclogs pores reducing blackheads and pore size appearance");
+		System.out.println("CALMS INFLAMED SKIN: Allantoin in this salicylic acid face wash has soothing properties. With every use, it calms inflammation and redness.");
+		System.out.println("PARABEN AND SLS FREE: Bodywise Salicylic Acid face wash is Dermatologist recommended gel-based face wash. Alcohol Free & sulphate Free");
+	
+	}
+
+}
