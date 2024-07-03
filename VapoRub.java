d3dace5199b20006e45e5786a55aaedc03f58da7
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 18:42:04 +0530

    basic code

---
+++ VapoRub.java
@@ -0,0 +1,13 @@
+class VapoRub {
+	
+	public static void main(String[] vicks) {
+		System.out.println("Get instant relief from 6 Cough and Cold symptoms");
+		System.out.println("Relieves Cough, nasal congestion, body ache, headache, muscular stiffness and body ache");
+		System.out.println("Suitable for steam inhalation on adults and children more than 6 years. Take one scoop of Vicks Vaporub and add it to hot water and inhale the vapors to get instant relief from the 6 Cold and Cough symptoms");
+		System.out.println("Suitable for adults and children above 2 years old");
+		System.out.println("Contains natural ingredients like Menthol, Camphor and Eucalyptus Oil known for their therapeutic benefits in cold and cough relief");
+		
+		
+	}
+
+}
