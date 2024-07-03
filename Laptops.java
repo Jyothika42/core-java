9f5c091b5d5bcd5a349364e67cb9166398164271
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 19:03:46 +0530

    basic code

---
+++ Laptops.java
@@ -0,0 +1,14 @@
+class Laptops{
+	
+	public static void main(String[] hp) {
+		System.out.println("zbookg3-32GB-4TB");
+		System.out.println("Operating System: Windows 10 Pro");
+		System.out.println("6th Gen Intel Core i7-6700HQ");
+		System.out.println("Color: Grey Bluetooth : Yes Dedicated Graphic : Nvidia M1000M Numeric Keyboard");
+		System.out.println("This product is backed by the Amazon Renewed Guarantee");
+	
+	}
+
+}
+
+			
