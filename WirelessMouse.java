45c24a89525b69371ed46c77c990ec0d348c1ebf
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 18:45:09 +0530

    basic code

---
+++ WirelessMouse.java
@@ -0,0 +1,14 @@
+class WirelessMouse{
+	
+	public static void main(String[] hp) {
+		System.out.println("COMPATIBLE OS Experience hassle-free transitions across different workstations or devices with multi-OS compatibility, supporting Windows 11/10 and macOS");
+		System.out.println("6 BUTTONS Ensure smooth workflow and make your everyday tasks easier with 6 versatile buttons, a 3D scroll wheel, and a DPI switch button to fit the way you work.");
+		System.out.println("SPEED AND PRECISION Elevate your computing experience with up to 1600 dpi. Enjoy an optical sensor that delivers accurate, fast tracking to keep you ahead of your work.");
+		System.out.println("RELIABLE CONNECTIONS Stay connected effortlessly with a mouse that seamlessly links to any device through a USB-A nano dongle and a 2.4 GHz wireless connection.");
+		System.out.println("ERGONOMIC DESIGN Maximize your productivity and click effortlessly with an ergonomically designed mouse for all-day comfort.");
+		System.out.println("LONG-LASTING BATTERY Boost your productivity with uninterrupted work using just 1 x AA battery, lasting up to 12 months. Enjoy worry-free clicks with the smart power system.");
+		System.out.println("3-YEAR WARRANTY Gain peace of mind by investing in this mouse, backed by a 3-year warranty and 24x7 online support. Refer to the HP Customer Support Center for more details.");
+		System.out.println("IN THE BOX HP M120 Wireless Mouse, USB-A nano dongle, 1 x AA battery, Quick Start Guide, and Warranty card are included in the box");
+	}
+
+}
