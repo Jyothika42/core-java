817829fa1661401eeea8fddc166bd0eda5be1f72
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 10:45:27 +0530

    my first code

---
+++ Pen.java
@@ -0,0 +1,12 @@
+class Pen {
+	
+	public static void main(String[] reynolds) {
+		System.out.println("Tip Size: 0.7 mm");
+		System.out.println("Ink Colour: Blue");
+		System.out.println("Pressure Free Writing: Our ball pens are designed to give a perfect grip that enables pressure-free writing.");
+		System.out.println("Perfect for Long Writing: Reynolds Ball Pen is lightweight which makes it perfect for long hours of writing.");
+		System.out.println("Fine Writing: The ball point pen uses a laser tip that enables fine writing.");
+		
+	}
+
+}
