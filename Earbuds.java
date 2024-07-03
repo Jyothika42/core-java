79bfb9ed4d162f631697a042e7839b04c0ebeead
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 19:04:33 +0530

    basic code

---
+++ Earbuds.java
@@ -0,0 +1,12 @@
+class Earbuds {
+	
+	public static void main(String[] Onepluse) {
+		System.out.println("[Enhanced Sound Experience]: The buds comes with 12.4mm driver unit, which delivers crisp clear and enhanced bass quality sound experience");
+		System.out.println("[Sound Master Equalizers]: For the OnePlus Nord Buds 2r, you get to choose how heavy or light you want your sound with the help of sound master equalizer s 3 unique audio profiles -Bold, Bass & Balanced");
+		System.out.println("[Battery Life]:The flagship-level battery life for the all-new OnePlus Nord Buds 2r delivers up to 38 hrs of non-stop music on a single charge.; [IP55 rating]: IP55 Water & Sweat Resistance");
+		System.out.println("[Gaming Mode]: It allows users to access a plethora of features while playing supported games on a OnePlus handset. They can be accessed at any time while playing games and give players the option to toggle different features and settings");
+		System.out.println("Control Type: Call Control; Model: E510a");
+	
+	}
+
+}
