939af7e22a529cd411d296714da94e11f5f2cadc
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 19:06:38 +0530

    basic code

---
+++ StainlessSteelWaterBottle.java
@@ -0,0 +1,15 @@
+class StainlessSteelWaterBottle {
+
+	public static void main(String[] Solimo){
+		System.out.println("Stainless Steel water bottle set for home and office use");
+		System.out.println("Made using high quality stainless steel for added durability and long life");
+		System.out.println("Features a spill-proof design; has a snug fitting lid for easy usage and prevention of leaks");
+		System.out.println("Made from 100% food grade materials to be safe for everyday usage");
+		System.out.println("Ergonomic design for comfortable grip and rust free body");
+		System.out.println("Ideal for storing water, iced tea, juices, shakes and more");
+		System.out.println("Dimensions: L x W x H: 7.3 x 7.3 x 26 cm");
+		System.out.println("Package Contents:- Solimo Stainless Steel Water Bottle Set of 3, Weight: 200 gm each, Colour: Silver, Capacity: 1 litre each");
+		
+	}
+
+}
