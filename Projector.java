2aa242fec443760f6ad72fbcfc71cee62219ba16
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 18:18:40 +0530

    basic code on Projector

---
+++ Projector.java
@@ -0,0 +1,10 @@
+class Projector {
+
+	public static void main(String[] Epson){
+		System.out.println("Projector is a device");
+		System.out.println("Projector is an optical device");
+		System.out.println("Projector helps to create large representation of the content");
+		
+	}
+
+}
