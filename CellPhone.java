6b6e9104bb5fad96d40e9945777d7b5f7cfb2028
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 18:15:04 +0530

    my first basic code on charger

---
+++ CellPhone.java
@@ -0,0 +1,10 @@
+class CellPhone {
+		public static void main(String[] motorola){
+			System.out.print("Cell Phone is Used for Communication \n");
+			System.out.print("Cell Phone is essential thing in everyday life ");
+			
+		}
+
+}
+
+
