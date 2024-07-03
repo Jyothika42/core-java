bf63c280184e39be91ddeb355b1da01640c817ef
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 18:51:41 +0530

    basic code

---
+++ Brush.java
@@ -0,0 +1,11 @@
+class Brush {
+	
+	public static void main(String[] colgate) {
+		System.out.println("Colgate Super Flexi comes with a flexible neck which helps clean hard-to-reach corners of the mouth");
+		System.out.println("Tongue cleaner to gently remove odour causing germs");
+		System.out.println("Helps sweep away food and plaque from along the gumline");
+		System.out.println("Slim ergonomic design for deep cleaning");
+		
+	}
+
+}
