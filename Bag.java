a05163f0e7d7d7dc6ef75596e238afc19d63162a
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-03 18:54:33 +0530

    basic code

---
+++ Bag.java
@@ -0,0 +1,11 @@
+class Bag {
+	
+	public static void main(String[] skybags) {
+		System.out.println("Material: Polyester");
+		System.out.println("Closer: Zip");
+		System.out.println("Lightweight: Our products are strive to be lightweight to minimize the overall weight and strain of carrying them, particularly during travel");
+		System.out.println("Durability: Our products are designed to be durable and long-lasting, able to withstand the rigors of everyday use or the demands of travel without easily succumbing to wear and tear.");
+		System.out.println("Easy Maintenance: Ourproducts are easy to clean and maintain, with materials that are resistant to stains or can be easily wiped clean.");
+	}
+
+}
