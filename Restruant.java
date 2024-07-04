9d696f46dd21e4c4e485fa49abb62790c8d6f641
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-04 10:33:58 +0530

    commiting array related code

---
+++ Restruant.java
@@ -0,0 +1,20 @@
+class Restruant{
+public static void main(String [] food){
+	
+System.out.println("Main started"); 
+String restruantName = "Kalpaturu Grand";
+String place = "Tiptur";
+String foodMenu[] = {"Idly","Dosa","Pongal", "Upma" ,"Pudina Rice" ,"Pulao" ,"Fried Rice", "Paratha", "Thali" ,"Sandwich", "Milkshake", "Juice", "Manchurian", "Samosa", "Vadapav", "Pavbhaji", "Soup", "Vada", "Poori", "Coffee", "Tea" ,"Panipuri" ,"Salad" ,"Pakoda", "Puliyogare Rice"};
+  
+for(String restruant : restruantName){
+	 System.out.println("restruantName");
+ }
+	
+
+
+System.out.println("Main ended");
+
+}
+
+
+}
