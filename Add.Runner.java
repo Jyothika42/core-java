65be06c3aa7a968a339a14f5f36fb15c3f5ae753
Jyothika42 <jyothika.xworkz@gmail.com>
2024-07-17 22:22:38 +0530

    Technical drill 17/07/2024

--- Add.Runner.java
+++
@@ -1,7 +0,0 @@
-class AddRunner{
-
-	public static void main(String[] args)
-	AdditionByte.add(10,2);
-	AdditionByte.add(12,25);
-	
-}
---
+++ AddRunner.java
@@ -0,0 +1,8 @@
+class AddRunner {
+    public static void main(String[] args) {
+        Add.add((byte) 25, (byte) 34);
+        Add.add((short) 2, (short) 98,(short)45,(short)65);
+		
+	
+    }
+}
